e71b2efbccac1398526edc3d5dc615684f38bd87
ishibashi.futos <ishibashi.futos@outlook.com>
2020-05-13 13:36:56 +0900

    FlexBoxの配置方法のサンプルを追記

--- calculator/App.tsx
+++ calculator/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StyleProp } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.pattern1}>
+        <View style={{width: 100, height: 100, backgroundColor: 'blue'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'red'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'gray'}} />
+      </View>
+      <View style={styles.pattern2}>
+        <View style={{width: 100, height: 100, backgroundColor: 'blue'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'red'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'gray'}} />
+      </View>
+      {/* 横軸いっぱい確保しているので、Widthを削除 */}
+      <View style={styles.pattern3}>
+        <View style={{height: 100, backgroundColor: 'blue'}} />
+        <View style={{height: 100, backgroundColor: 'red'}} />
+        <View style={{height: 100, backgroundColor: 'gray'}} />
+      </View>
     </View>
   );
 }
@@ -12,8 +27,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  pattern1: {
+    flex: 1,
+    /** 縦軸方向に整列 */
+    flexDirection: 'column',
+    /** 縦軸は等間隔に整列 */
+    justifyContent: 'space-between',
+    /** 横軸は横のスタート位置へ */
+    alignItems: 'flex-start',
+  },
+  pattern2: {
+    flex: 1,
+    /** 縦軸方向に整列 */
+    flexDirection: 'column',
+    /** 縦軸は中央より均等に並ぶように */
+    justifyContent: 'space-around',
+    /** 横軸は終端へ */
+    alignItems: 'flex-end',
+  },
+  pattern3: {
+    flex: 1,
+    /** 縦軸方向に整列 */
+    flexDirection: 'column',
+    /** 縦軸は中央寄せ */
     justifyContent: 'center',
+    /** 横軸は横軸いっぱいに広げる */
+    alignItems: 'stretch',
   },
 });
